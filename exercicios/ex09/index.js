f64dffdc951bc88109584697d64e1c69ed1856fd
kalebemax <169088614+kalebemax@users.noreply.github.com>
2024-07-08 15:27:59 -0300

    criei o ex09

---
+++ exercicios/ex09/index.js
@@ -0,0 +1,12 @@
+var vel = 84.5
+console.log(`A sua velocidade foi de ${vel}Km/h`)
+
+
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade. MULTADO.!')
+    console.log('Dirija com mais cuidado!')
+}
+else {
+    console.log('Parabéns, Continue respeitando o limite de velocidade!')
+}
+
