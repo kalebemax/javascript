a3dfb2ce988fa8ffbba9b5df663ad619e6e60050
kalebemax <169088614+kalebemax@users.noreply.github.com>
2024-07-10 11:58:50 -0300

    ex10 concluído

--- exercicios/ex10/script.js
+++ exercicios/ex10/script.js
@@ -1,14 +1,21 @@
 function carregar() {
-    var msg = document.querySelector('#msg')
-    var img = document.querySelector('.img')
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    hora = 17
+    var min = data.getMinutes()
+
     if (hora >= 0 && hora < 12) {
-        msg.innerHTML = `<p>Bom Dia!</p>Agora são <strong>${hora}</strong> horas.`
+        msg.innerHTML = `<p>Bom Dia!</p>Agora são <strong>${hora}</strong> horas e <strong>${min}</strong> minutos.`
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = '#585206'
     } else if (hora >= 12 && hora <= 18) {
-        msg.innerHTML = `<p>Boa Tarde!</p> Agora são <strong>${hora}</strong> horas.`
-    } else (hora >= 19) 
-        msg.innerHTML = `<p>Boa Noite!</p> Agora são <strong>${hora}</strong> horas.`
-        
+        msg.innerHTML = `<p>Boa Tarde!</p> Agora são <strong>${hora}</strong> horas e <strong>${min}</strong> minutos.`
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#EBA05C'
+    } else {
+        msg.innerHTML = `<p>Boa Noite!</p> Agora são <strong>${hora}</strong> horas e <strong>${min}</strong> minutos.`
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = '#694161'
+    }
 }
