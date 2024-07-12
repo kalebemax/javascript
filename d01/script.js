e9937d22826c2e5e5037781a4e5b189bf10acf41
kalebemax <169088614+kalebemax@users.noreply.github.com>
2024-07-12 17:31:30 -0300

    ex12 terminado

--- d01/script.js
+++
@@ -1,4 +0,0 @@
-var numb = 100
-for(var numb;numb <= 50;numb++) {
-    console.log(`${numb}`)
-}
--- exercicios/ex12/script.js
+++ exercicios/ex12/script.js
@@ -2,20 +2,40 @@ function contar() {
     var start = document.querySelector('#num')
     var end = document.querySelector('#fnum')
     var pass = document.querySelector('#pnum')
-    var txt = document.querySelector('.cont')
-    var res = Number(start.value) 
-    var res1 = Number(end.value)
-    
-    for(start.value;start >= 0;start++) {
-        txt.innerHTML = `${res}`
+    var txt = document.querySelector('.cont') 
+    var res = Number(end.value)
+    var res1 = Number(start.value)
+    var res2 = Number(pass.value)
+
+    if (res2 <= 0) {
+        alert('Passo inválido! Considerando Passo 1')
+        res2 = 1
     }
 
-    for(end.value;end > 0;end++) {
-        txt.innerHTML = `${res1}`
-    }
-
-    if (start.value == 0) {
-        alert('[Erro] Insira um valor inicial!')
-    }
+    if (start.value.length == 0, end.value.length == 0, pass.value.length == 0) {
+       txt.innerHTML = 'Impossível contar!'
+    } else {
+        txt.innerHTML = '<p>Contando...</p>'
+      if (res1 < res) {
+        for(var c = res1; c <= res; c += res2) {
+            txt.innerHTML += `${c} \u{1F449}`
+        } 
+           
+    } 
+        if (res > res1) {
+        for(var c = res1; c >= res; c -= res2) {
+            txt.innerHTML += `${c}`
+       } 
+    }  
 } 
+        txt.innerHTML += `\u{1F3C1}`
+} 
+
+   
+     
+
+    
+
+
+
  
